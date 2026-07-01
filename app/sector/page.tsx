5b1acf09cf71049ab5c14efc6a57b00293500197
Marcus Vinicius <marcusv892@gmail.com>
2026-07-01 18:18:32 -0300

    feat, refactor: create and refactor pages application

--- app/sector/page.tsx
+++ app/sector/page.tsx
@@ -1,16 +1,19 @@
-import ButtonTitle from "@/components/ButtonTitle";
+import DialogComponentButton from "@/components/DialogComponentButton";
 import TitlePage from "@/components/TitlePage";
 
 export default function SectorPage() {
   return (
     <div>
-      <div className="flex justify-between items-center">
+      <div className="flex items-center justify-between">
         <TitlePage
           title="Gerenciamento de Setores"
           description="Visualize e organize as unidades estruturais da organização."
         />
 
-        <ButtonTitle title="Novo Setor" />
+        <DialogComponentButton
+          dialogHeader="Novo Setor"
+          dialogDescription="Preencha as informações para criar um novo setor."
+        />
       </div>
     </div>
   );
