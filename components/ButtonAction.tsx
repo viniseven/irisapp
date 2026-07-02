f9ae93b2f855a38df200924c47693df90d47e402
Marcus Vinicius <marcusv892@gmail.com>
2026-07-02 12:03:03 -0300

    refactor: button component props

--- components/ButtonAction.tsx
+++ components/ButtonAction.tsx
@@ -1,37 +1,14 @@
+import { ComponentPropsWithoutRef } from "react";
 import { Button } from "./ui/button";
 
-interface ButtonTitleProps {
+interface ButtonTitleProps extends ComponentPropsWithoutRef<typeof Button> {
   title?: string;
-  type?: "button" | "submit" | "reset" | undefined;
   children?: React.ReactNode;
-  form?: string;
-  onClick?: () => void; // Declarado como opcional
-  className?: string;
-  variant?:
-    | "default"
-    | "destructive"
-    | "outline"
-    | "secondary"
-    | "ghost"
-    | "link"
-    | undefined;
 }
 
-export default function ButtonAction({
-  type,
-  children,
-  form,
-  onClick, // 1. Recebe o onClick aqui
-  variant, // 2. Recebe a variant aqui
-}: ButtonTitleProps) {
+export default function ButtonAction({ children, ...props }: ButtonTitleProps) {
   return (
-    <Button
-      variant={variant}
-      onClick={onClick}
-      type={type}
-      form={form}
-      className="px-6 py-6 font-bold hover:cursor-pointer"
-    >
+    <Button {...props} className="px-6 py-6 font-bold hover:cursor-pointer">
       {children}
     </Button>
   );
