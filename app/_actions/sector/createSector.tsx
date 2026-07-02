90873966acb7d1c1fdc45663c1143aa0f71a80a1
Marcus Vinicius <marcusv892@gmail.com>
2026-07-02 12:02:31 -0300

    refactor: server action prisma adapter

--- app/_actions/sector/createSector.tsx
+++ app/_actions/sector/createSector.tsx
@@ -1,10 +1,10 @@
 "use server";
 
 import { FormSchema } from "@/app/sector/_components/FormSectorComponent";
-import { db } from "@/lib/prisma";
+import { prisma } from "@/lib/prisma";
 
 export async function createSector({ name }: FormSchema) {
-  await db.sector.create({
+  await prisma.sector.create({
     data: {
       name,
     },
