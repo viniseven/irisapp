e474b25cedf3f9c61039ed7058287e25ba74f9e8
Marcus Vinicius <marcusv892@gmail.com>
2026-07-08 00:26:19 -0300

    feat: add table component in page

--- app/sector/page.tsx
+++ app/sector/page.tsx
@@ -1,9 +1,15 @@
 import DialogComponentButton from "@/components/DialogComponentButton";
 import TitlePage from "@/components/TitlePage";
+import DataTable from "./_components/dataTable";
+import { columns } from "./_components/columns";
+import { getSectorEmployeeCount } from "../_dataAccess/sector/getSector";
+import MetricsCard from "@/components/MetricsCard";
 
-export default function SectorPage() {
+export default async function SectorPage() {
+  const sectorsData = await getSectorEmployeeCount();
+
   return (
-    <div>
+    <div className="flex flex-col gap-10">
       <div className="flex items-center justify-between">
         <TitlePage
           title="Gerenciamento de Setores"
@@ -14,6 +20,14 @@ export default function SectorPage() {
           dialogHeader="Novo Setor"
           dialogDescription="Preencha as informações para criar um novo setor."
         />
+      </div>
+      <div className="flex gap-8">
+        <MetricsCard title="TOTAL DE SETORES" metric={12} />
+        <MetricsCard title="COLABORADORES ATIVOS" metric={220} />
+        <MetricsCard title="TAXA DE TREINAMENTO" metric={84.2} />
+      </div>
+      <div>
+        <DataTable columns={columns} data={sectorsData} />
       </div>
     </div>
   );
