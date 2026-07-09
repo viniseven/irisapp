a8556bc5bed1677ef6b62432eb9a086cbd0dd700
Marcus Vinicius <marcusv892@gmail.com>
2026-07-08 22:55:24 -0300

    feat: metrics card for pages

--- components/MetricsCard.tsx
+++ components/MetricsCard.tsx
@@ -1,11 +1,18 @@
 interface MetricsCardProps {
   title: string;
-  metric: number;
+  metric: string;
+  className: string;
 }
 
-export default function MetricsCard({ title, metric }: MetricsCardProps) {
+export default function MetricsCard({
+  title,
+  metric,
+  className,
+}: MetricsCardProps) {
   return (
-    <div className="flex flex-col gap-1 rounded-lg bg-white p-8">
+    <div
+      className={`${className} flex w-full flex-col gap-1 rounded-lg bg-white p-8`}
+    >
       <p className="text-text-muted text-xs font-semibold">{title}</p>
       <span className="text-5xl font-bold">{metric}</span>
     </div>
