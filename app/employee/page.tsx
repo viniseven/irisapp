e6d0a7afae8db24d8fd260d22cd224d3f48fad51
Marcus Vinicius <marcusv892@gmail.com>
2026-07-08 22:56:29 -0300

    feat, refactor: sector page and employee page

--- app/employee/page.tsx
+++ app/employee/page.tsx
@@ -1,16 +1,29 @@
-import ButtonTitle from "@/components/ButtonAction";
-import TitlePage from "@/components/TitlePage";
+import MetricsCard from "@/components/MetricsCard";
+import EmployeeHeader from "./_components/employeeHeader";
+import { formatPercent } from "@/lib/utils/formatPercent";
 
 export default function EmployeePage() {
   return (
-    <div>
-      <div className="flex items-center justify-between">
-        <TitlePage
-          title="Colaboradores"
-          description="Gerencie a base de colaboradores e status de capacitação."
+    <div className="flex flex-col gap-10">
+      <EmployeeHeader />
+      <div className="flex justify-between gap-28">
+        <MetricsCard
+          title="TOTAL DE SETORES"
+          metric={12}
+          className="border-l-blue-dark border-l-4"
         />
-        <ButtonTitle title="Novo Colaborador" />
+        <MetricsCard
+          title="COLABORADORES ATIVOS"
+          metric={220}
+          className="border-l-yellow-info border-l-4"
+        />
+        <MetricsCard
+          title="TAXA DE TREINAMENTO"
+          metric={formatPercent.format(8)}
+          className="border-l-red border-l-4"
+        />
       </div>
+      <div></div>
     </div>
   );
 }
--- app/sector/page.tsx
+++ app/sector/page.tsx
@@ -1,30 +1,32 @@
-import DialogComponentButton from "@/components/DialogComponentButton";
-import TitlePage from "@/components/TitlePage";
 import DataTable from "./_components/dataTable";
 import { columns } from "./_components/columns";
 import { getSectorEmployeeCount } from "../_dataAccess/sector/getSector";
 import MetricsCard from "@/components/MetricsCard";
+import { formatPercent } from "@/lib/utils/formatPercent";
+import SectorHeader from "./_components/sectorHeader";
 
 export default async function SectorPage() {
   const sectorsData = await getSectorEmployeeCount();
 
   return (
     <div className="flex flex-col gap-10">
-      <div className="flex items-center justify-between">
-        <TitlePage
-          title="Gerenciamento de Setores"
-          description="Visualize e organize as unidades estruturais da organização."
+      <SectorHeader />
+      <div className="flex justify-between gap-28">
+        <MetricsCard
+          title="TOTAL DE SETORES"
+          metric={12}
+          className="border-l-blue-dark border-l-4"
         />
-
-        <DialogComponentButton
-          dialogHeader="Novo Setor"
-          dialogDescription="Preencha as informações para criar um novo setor."
+        <MetricsCard
+          title="COLABORADORES ATIVOS"
+          metric={220}
+          className="border-l-yellow-info border-l-4"
         />
-      </div>
-      <div className="flex gap-8">
-        <MetricsCard title="TOTAL DE SETORES" metric={12} />
-        <MetricsCard title="COLABORADORES ATIVOS" metric={220} />
-        <MetricsCard title="TAXA DE TREINAMENTO" metric={84.2} />
+        <MetricsCard
+          title="TAXA DE TREINAMENTO"
+          metric={formatPercent.format(8)}
+          className="border-l-red border-l-4"
+        />
       </div>
       <div>
         <DataTable columns={columns} data={sectorsData} />
