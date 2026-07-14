f225ec7ba03cf90b4fb2bafe50f76c920df0cf7c
Marcus Vinicius <marcusv892@gmail.com>
2026-07-13 23:24:11 -0300

    refactor: added components metrics card

--- app/employee/page.tsx
+++ app/employee/page.tsx
@@ -1,27 +1,29 @@
 import MetricsCard from "@/components/MetricsCard";
 import EmployeeHeader from "./_components/employeeHeader";
 import { formatPercent } from "@/lib/utils/formatPercent";
+import { Users, CircleCheck, Clock, UserRoundX } from "lucide-react";
 
 export default function EmployeePage() {
   return (
     <div className="flex flex-col gap-10">
       <EmployeeHeader />
-      <div className="flex justify-between gap-28">
+      <div className="flex justify-between gap-3">
         <MetricsCard
-          title="TOTAL DE SETORES"
-          metric={12}
-          className="border-l-blue-dark border-l-4"
-        />
-        <MetricsCard
-          title="COLABORADORES ATIVOS"
-          metric={220}
-          className="border-l-yellow-info border-l-4"
-        />
-        <MetricsCard
-          title="TAXA DE TREINAMENTO"
-          metric={formatPercent.format(8)}
-          className="border-l-red border-l-4"
-        />
+          title="Total de Colaboradores"
+          value={12}
+          iconBgColor="bg-gray-100"
+        >
+          <Users className="text-blue-dark" />
+        </MetricsCard>
+        <MetricsCard title="Ativos" value={220} iconBgColor="bg-green-90">
+          <CircleCheck className="text-green-900" />
+        </MetricsCard>
+        <MetricsCard title="Afastados" value={220} iconBgColor="bg-yellow-90">
+          <Clock className="text-orange-800" />
+        </MetricsCard>
+        <MetricsCard title="Inativos" value={220} iconBgColor="bg-red-90">
+          <UserRoundX className="text-red" />
+        </MetricsCard>
       </div>
       <div></div>
     </div>
