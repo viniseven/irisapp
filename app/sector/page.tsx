7721bb3971d5f9375bf437d799d78405c2dc66af
Marcus Vinicius <marcusv892@gmail.com>
2026-07-14 23:58:32 -0300

    feat: added metricsCard component and page sector

--- app/sector/page.tsx
+++ app/sector/page.tsx
@@ -1,35 +1,48 @@
-import DataTable from "./_components/dataTable";
-import { columns } from "./_components/columns";
-import { getSectorEmployeeCount } from "../_dataAccess/sector/getSector";
+import { getAllSector } from "../_dataAccess/sector/getSector";
 import MetricsCard from "@/components/MetricsCard";
-import { formatPercent } from "@/lib/utils/formatPercent";
+import { Building2, Flame, GraduationCap, TrendingUp } from "lucide-react";
 import SectorHeader from "./_components/sectorHeader";
+import { DataTable } from "./_components/dataTable";
+import { columns } from "./_components/columns";
 
 export default async function SectorPage() {
-  const sectorsData = await getSectorEmployeeCount();
+  const data = await getAllSector();
 
   return (
     <div className="flex flex-col gap-10">
       <SectorHeader />
-      <div className="flex justify-between gap-28">
+      <div className="flex justify-between gap-3">
         <MetricsCard
-          title="TOTAL DE SETORES"
-          metric={12}
-          className="border-l-blue-dark border-l-4"
-        />
+          title="Total de Setores"
+          value={12}
+          iconBgColor="bg-gray-100"
+        >
+          <Building2 className="text-blue-dark" />
+        </MetricsCard>
         <MetricsCard
-          title="COLABORADORES ATIVOS"
-          metric={220}
-          className="border-l-yellow-info border-l-4"
-        />
+          title="Setor com maior engajamento"
+          value={220}
+          iconBgColor="bg-orange-90"
+        >
+          <Flame className="text-orange-800" />
+        </MetricsCard>
         <MetricsCard
-          title="TAXA DE TREINAMENTO"
-          metric={formatPercent.format(8)}
-          className="border-l-red border-l-4"
-        />
+          title="Média de conclusão"
+          value={220}
+          iconBgColor="bg-green-90"
+        >
+          <TrendingUp className="text-green-900" />
+        </MetricsCard>
+        <MetricsCard
+          title="Treinamentos ativos"
+          value={220}
+          iconBgColor="bg-red-90"
+        >
+          <GraduationCap className="text-red" />
+        </MetricsCard>
       </div>
       <div>
-        <DataTable columns={columns} data={sectorsData} />
+        <DataTable columns={columns} data={data} />
       </div>
     </div>
   );
