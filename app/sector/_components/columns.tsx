8580e6f677264c502b31aa6644e0149661a14cca
Marcus Vinicius <marcusv892@gmail.com>
2026-07-19 22:43:36 -0300

    feat: added data table component

--- app/sector/_components/columns.tsx
+++ app/sector/_components/columns.tsx
@@ -1,33 +1,120 @@
 "use client";
 
+import DropdownActions from "@/components/DropdownActions";
+import { Progress } from "@/components/ui/progress";
 import { ColumnDef } from "@tanstack/react-table";
+import { Building2 } from "lucide-react";
 
 export type SectorColumns = {
   sector: string;
   employeers: number;
   trainings: number;
   progress: number;
+  leader?: {
+    name: string;
+  };
 };
 
 export const columns: ColumnDef<SectorColumns>[] = [
   {
     accessorKey: "sector",
-    header: "Setor",
+    header: () => (
+      <div className="w-full pl-6 text-left font-semibold text-slate-500">
+        Setor
+      </div>
+    ),
+    cell: ({ row }) => {
+      const sectorData = row.original;
+      console.log(sectorData);
+      // Fallback caso você ainda não tenha o líder vindo do banco
+      const leaderName = sectorData.leader?.name || "Responsável não definido";
+
+      return (
+        <div className="flex items-center gap-3 pl-4">
+          <div className="flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-50 font-semibold text-indigo-600">
+            <Building2 className="h-5 w-5" />
+          </div>
+
+          <div className="flex flex-col">
+            <span className="text-sm leading-tight font-semibold text-slate-800">
+              Tecnologia da Informação
+            </span>
+            <div className="mt-1 flex items-center gap-1.5">
+              <span className="text-xs text-slate-500">{leaderName}</span>
+            </div>
+          </div>
+        </div>
+      );
+    },
   },
   {
     accessorKey: "employeers",
-    header: "Colaboradores",
+    header: () => (
+      <div className="text-center font-semibold text-slate-500">
+        Colaboradores
+      </div>
+    ),
+    cell: ({ row }) => {
+      const count = row.getValue("employeers") as number;
+      return (
+        <div className="flex flex-col text-center">
+          <span className="block text-sm font-bold text-slate-800">86</span>
+          <div>
+            <span className="text-xs text-slate-400">colaboradores</span>
+          </div>
+        </div>
+      );
+    },
   },
   {
     accessorKey: "trainings",
-    header: "Treinamentos",
+    header: () => (
+      <div className="text-center font-semibold text-slate-500">
+        Treinamentos
+      </div>
+    ),
+    cell: ({ row }) => {
+      const count = row.getValue("trainings") as number;
+      return (
+        <div className="flex flex-col text-center">
+          <span className="text-sm font-bold text-slate-800">10</span>
+          <div>
+            <span className="text-xs text-slate-400">treinamentos</span>
+          </div>
+        </div>
+      );
+    },
   },
   {
     accessorKey: "progress",
-    header: "Progresso",
+    header: () => (
+      <div className="pl-4 text-center font-semibold text-slate-500">
+        Progresso
+      </div>
+    ),
+    cell: ({ row }) => {
+      const value = row.getValue("progress") as number;
+
+      return (
+        <div className="flex flex-col gap-1.5 pl-4">
+          <div className="flex items-center justify-between text-xs">
+            <span className="font-medium text-slate-400">Conclusão</span>
+            <span className="font-bold text-slate-800">100%</span>
+          </div>
+          <Progress value={33} className="bg-green-600" />
+        </div>
+      );
+    },
   },
   {
-    accessorKey: "",
-    header: "Acções",
+    id: "actions",
+    header: () => <div />,
+    cell: ({ row }) => {
+      return (
+        <div className="text-right">
+          <DropdownActions />
+        </div>
+      );
+    },
   },
 ];
--- app/sector/_components/dataTable.tsx
+++ app/sector/_components/dataTable.tsx
@@ -2,8 +2,11 @@
 
 import {
   ColumnDef,
+  ColumnFiltersState,
   flexRender,
   getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
   useReactTable,
 } from "@tanstack/react-table";
 
@@ -15,6 +18,9 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import ButtonAction from "@/components/ButtonAction";
+import React from "react";
+import { Input } from "@/components/ui/input";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -25,56 +31,103 @@ export function DataTable<TData, TValue>({
   columns,
   data,
 }: DataTableProps<TData, TValue>) {
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    [],
+  );
+  console.log(columnFilters);
   const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
+    state: {
+      columnFilters,
+    },
   });
 
   return (
-    <div className="overflow-hidden rounded-md border">
-      <Table>
-        <TableHeader>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
-                return (
-                  <TableHead key={header.id}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext(),
-                        )}
-                  </TableHead>
-                );
-              })}
-            </TableRow>
-          ))}
-        </TableHeader>
-        <TableBody>
-          {table.getRowModel().rows?.length ? (
-            table.getRowModel().rows.map((row) => (
-              <TableRow
-                key={row.id}
-                data-state={row.getIsSelected() && "selected"}
-              >
-                {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </TableCell>
-                ))}
+    <div className="flex flex-col gap-2">
+      <div>
+        <Input
+          placeholder="Filtrar setor..."
+          value={(table.getColumn("sector")?.getFilterValue() as string) ?? ""}
+          onChange={(event) =>
+            table.getColumn("sector")?.setFilterValue(event.target.value)
+          }
+          className="max-w-sm border border-none bg-white shadow-md"
+        />
+      </div>
+      <div className="overflow-hidden rounded-md bg-white shadow-md">
+        <Table>
+          <TableHeader className="border-none">
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id} className="border-none">
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id} className="text-text-muted">
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext(),
+                          )}
+                    </TableHead>
+                  );
+                })}
               </TableRow>
-            ))
-          ) : (
-            <TableRow>
-              <TableCell colSpan={columns.length} className="h-24 text-center">
-                No results.
-              </TableCell>
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                  className="border-background border"
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext(),
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  Não há nenhum registro
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <ButtonAction
+          variant="outline"
+          size="sm"
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          Anterior
+        </ButtonAction>
+        <ButtonAction
+          variant="outline"
+          size="sm"
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          Próximo
+        </ButtonAction>
+      </div>
     </div>
   );
 }
