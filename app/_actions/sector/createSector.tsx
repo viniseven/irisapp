a57b12be9d4de602536e7c8459c2e0eb0c589840
Marcus Vinicius <marcusv892@gmail.com>
2026-07-20 23:32:46 -0300

    refactor: server action service isolating return

--- app/_actions/sector/createSector.tsx
+++ app/_actions/sector/createSector.tsx
@@ -1,12 +1,27 @@
 "use server";
 
+import { getFindSector } from "@/app/_dataAccess/sector/getSector";
 import { FormSchema } from "@/app/sector/_components/formSectorComponent";
 import { prisma } from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
 
-export async function createSector({ name }: FormSchema) {
-  await prisma.sector.create({
-    data: {
-      name,
-    },
-  });
+export async function createSector(data: FormSchema) {
+  try {
+    const resultFindSector = await getFindSector(data.sector);
+
+    if (resultFindSector) {
+      return { success: false, message: "Já existe um setor com este nome" };
+    }
+
+    await prisma.sector.create({
+      data: {
+        name: data.sector,
+        managerId: data.managerId,
+      },
+    });
+    revalidatePath("/sector");
+    return { success: true, message: "Setor cadastrado com sucesso" };
+  } catch (error) {
+    return { success: false, message: "Erro interno do servidor" };
+  }
 }
