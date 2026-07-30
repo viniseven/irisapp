64f284f32a48d71254a4597cb3d5cd382adc4ace
Marcus Vinicius <marcusv892@gmail.com>
2026-07-30 00:30:57 -0300

    feat: create dropdownaction component

--- components/DropdownActions.tsx
+++ components/DropdownActions.tsx
@@ -6,29 +6,42 @@ import {
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
 import ButtonAction from "./ButtonAction";
+import { useState } from "react";
+import { Dialog } from "./ui/dialog";
 
-export default function DropdownActions() {
+interface DropDownActionsComponentProps {
+  children: React.ReactNode;
+}
+
+export default function DropDownActions({
+  children,
+}: DropDownActionsComponentProps) {
+  const [dialogIsOpen, setDialogIsOpen] = useState(false);
+
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <ButtonAction
-          variant={"outline"}
-          className="h-8 w-8 rounded-full p-0 transition-colors hover:bg-slate-200"
-        >
-          <MoreHorizontal className="h-4 w-4 text-slate-500" />
-        </ButtonAction>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" className="w-40 bg-white">
-        <DropdownMenuItem className="cursor-pointer">
-          Editar Setor
-        </DropdownMenuItem>
-        <DropdownMenuItem className="cursor-pointer">
-          Ver Detalhes
-        </DropdownMenuItem>
-        <DropdownMenuItem className="cursor-pointer text-red-600 focus:bg-red-50 focus:text-red-600">
-          Excluir
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Dialog open={dialogIsOpen} onOpenChange={setDialogIsOpen}>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <ButtonAction
+            variant={"outline"}
+            className="h-8 w-8 rounded-full p-0 transition-colors hover:bg-slate-200"
+          >
+            <MoreHorizontal className="h-4 w-4 text-slate-500" />
+          </ButtonAction>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className="w-40 bg-white">
+          <DropdownMenuItem
+            className="cursor-pointer"
+            onSelect={() => setDialogIsOpen(true)}
+          >
+            Editar Setor
+          </DropdownMenuItem>
+          <DropdownMenuItem className="cursor-pointer text-red-600 focus:bg-red-50 focus:text-red-600">
+            Excluir
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      {children}
+    </Dialog>
   );
 }
